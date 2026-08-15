9c5f4228c003cec0cde060d46437f8f064847eae
João Gabriel Tavares de Lira <joaogab.lira24@gmail.com>
2026-08-14 23:03:21 -0300

    Aula 02

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from database import Base, engine, get_db
 from models import ProdutoDB
 from schemas import ProdutoCreate, ProdutoResponse
+from fastapi import HTTPException
 
 Base.metadata.create_all(bind=engine) # cria as tabelas, se ainda não existirem
 app = FastAPI()
@@ -11,10 +12,27 @@ app = FastAPI()
 def listar_produtos(db: Session = Depends(get_db)):
     return db.query(ProdutoDB).all()
 
+#GET /produtos/{id} -> retorna um único produto pelo id
+@app.get('/produtos/{produto_id}', response_model=ProdutoResponse)
+def obter_produto(produto_id: int, db: Session = Depends(get_db)):
+    produto = db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
+    if produto is None:
+        raise HTTPException(status_code=404, detail='Produto não encontrado')
+    return produto
+
 @app.post('/produtos', response_model=ProdutoResponse, status_code=201)
 def criar_produto(produto: ProdutoCreate, db: Session = Depends(get_db)):
     novo_produto = ProdutoDB(**produto.dict())
     db.add(novo_produto)
     db.commit()
     db.refresh(novo_produto)
-    return novo_produto
+    return novo_produ
+
+# DELETE /produtos/{id} -> remove um produto do banco de dados
+@app.delete('/produtos/{produto_id}', status_code=204)
+def remover_produto(produto_id: int, db: Session = Depends(get_db)):
+    produto = db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
+    if produto is None:
+        raise HTTPException(status_code=404, detail='Produto não encontrado')
+    db.delete(produto)
+    db.commit()
